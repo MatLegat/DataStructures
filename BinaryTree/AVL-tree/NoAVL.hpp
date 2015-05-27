7f2233f37c4d75a41d3fb523ca6955f5f483e079
Matteus Legat <MatLegat@users.noreply.github.com>
2015-05-26 23:00:01 -0300

    Update NoAVL.hpp

--- BinaryTree/AVL-tree/NoAVL.hpp
+++ BinaryTree/AVL-tree/NoAVL.hpp
@@ -14,38 +14,40 @@ class NoAVL {
     std::vector<NoAVL<T>* > elementos;
 
  public:
-    explicit NoAVL(const T& dado) {
-        altura = 1;
-        this->dado = new T(dado);
+    explicit NoAVL(const T& dado) : dado(new T(dado)) {
+        altura = 0;
         esquerda = NULL;
         direita = NULL;
     }
 
-    virtual ~NoAVL() {}
+    virtual ~NoAVL() {
+        if (esquerda != NULL)
+		    esquerda->~NoAVL();  // Destroi subarvore esquerda
+	    if (direita != NULL)
+		    direita->~NoAVL();  // Destroi subarvore direita
+	    delete dado;
+    }
 
     int getAltura() {
         return altura;
     }
 
+    int getAltura(NoAVL<T>* nodo) {
+        if (nodo == NULL)
+            return -1;  // Se nodo nao existe
+        else
+            return nodo->getAltura();
+    }
+
     void updateAltura(NoAVL<T> *arv) {
         int alturaMaiorFilho;
-        if (arv->esquerda == NULL) {
-            if (arv->direita == NULL)
-                alturaMaiorFilho = 0;
-            else
-                alturaMaiorFilho = arv->direita->getAltura();
-        } else if (direita == NULL) {
-            alturaMaiorFilho = arv->esquerda->getAltura();  // esquerda!=NULL
-        } else {  // Nenhum null
-            int alturaDireita = direita->getAltura();
-            int alturaEsquerda = esquerda->getAltura();
-            if (alturaDireita > alturaEsquerda)
-                alturaMaiorFilho = alturaDireita;
-            else
-                alturaMaiorFilho = alturaEsquerda;
+        if (getAltura(arv->esquerda) > getAltura(arv->direita)) {
+            // Maior é o esquerdo
+            alturaMaiorFilho = getAltura(arv->esquerda);
+        } else {
+            alturaMaiorFilho = getAltura(arv->direita);
         }
-        arv->altura = 1 + alturaMaiorFilho;
-        //  return altura = 1 + alturaMaiorFilho;
+        arv->altura = alturaMaiorFilho + 1;
     }
 
     std::vector<NoAVL<T>* > getElementos() {
@@ -60,47 +62,43 @@ class NoAVL {
         return direita;
     }
 
-    int getBalanco(NoAVL<T>* arv) {
-        if (arv->esquerda == NULL) {
-            if (arv->direita == NULL)
-                return 0;
-            else
-                return -1 * arv->direita->altura;
-        } else if (arv->direita == NULL) {
-            return arv->esquerda->altura;
-        } else {
-            return arv->esquerda->altura - arv->direita->altura > 1;
-        }
-    }
-
     NoAVL<T>* inserir(const T& dado, NoAVL<T>* arv) {
-	    if (arv == NULL) {
-	        return new NoAVL<T>(dado);
-        } else if (dado < *arv->getDado()) {
-            // Inserção à esquerda.
-            arv->esquerda = inserir(dado, arv->esquerda);
-            if (getBalanco(arv) > 1) {
-                if (dado < *arv->esquerda->dado)
-                    arv = rodaDireita(arv);
-                else
-                    arv = rodaDireitaDuplo(arv);
+	    NoAVL<T> *t;  // auxiliar
+	    if (dado < *arv->getDado()) {  // Inserção à esquerda.
+	        if (arv->esquerda == NULL) {  // Local para inserir / último nodo
+	            t = new NoAVL<T>(dado);
+	            if (t == NULL) {
+	                throw "BAD ALLOC";
+                }
+                arv->esquerda = t;
+            } else {  // nao chegou no fim onde ira inserir
+                t = arv->inserir(dado, arv->esquerda);
+                arv = equilibra(arv, dado);
+			    if (arv->getDado() != t->getDado()) {
+			        arv->esquerda = t;
+		        }
             }
-        } else {
-            // direita
-            arv->direita = inserir(dado, arv->direita);
-            if (getBalanco(arv) > 1) {
-                if (dado > *arv->direita->dado)
-                    arv = rodaEsquerda(arv);
-                else
-                    arv = rodaEsquerdaDuplo(arv);
+        } else {  // direita
+	        if (arv->direita == NULL) {  // Local para inserir / último nodo
+	            t = new NoAVL<T>(dado);
+	            if (t == NULL) {
+	                throw "BAD ALLOC";
+	                }
+                arv->direita = t;
+            } else {  // nao chegou no fim onde ira inserir
+                t = arv->inserir(dado, arv->direita);
+                arv = equilibra(arv, dado);
+			    if (arv->getDado() != t->getDado()) {
+			        arv->direita = t;
+		        }
             }
         }
     	updateAltura(arv);
         return arv;
 	}
-
-    NoAVL<T>* remover(NoAVL<T>* arv, const T& dado) {
-		NoAVL<T> *t;  // Auxiliar
+//--------------------------------- ERRADO -----------------------------------
+    NoAVL<T>* remover(NoAVL<T>* arv, const T& dado) { 
+		/*NoAVL<T> *t;  // Auxiliar
 		if (arv == NULL)
 		    arv = new NoAVL<T>(dado);
 		    return arv;
@@ -117,7 +115,7 @@ class NoAVL {
 				for (t = arv->direita; t->esquerda != NULL; t = t->esquerda) {}
 				arv->dado = t->dado;
 				arv->direita = remover(arv->direita, *t->dado);
-				return equilibra(arv);
+ 				return equilibra(arv);
 			}
 		}
 
@@ -126,8 +124,9 @@ class NoAVL {
 		else
 		    arv->direita = remover(arv->direita, dado);
 
-		return equilibra(arv);
+	 	return equilibra(arv);  /*
 	}
+//----------------------------------------------------------------------------
 
     NoAVL<T> *rodaEsquerda(NoAVL<T> *arv) {
 		NoAVL<T> *t = arv->esquerda;  // Auxiliar
@@ -157,37 +156,28 @@ class NoAVL {
         return rodaDireita(arv);
     }
 
-    NoAVL<T>* equilibra(NoAVL<T> *arv) {
-    	if (getBalanco(arv) > 1) {
-    		if (getBalanco(arv->esquerda) > 0) {
-	    		arv = rodaEsquerda(arv);
-		    } else {
-			    arv = rodaEsquerdaDuplo(arv);
-		    }
-	    } else if (getBalanco(arv) < -1) {
-	    	if (getBalanco(arv->direita) > 0) {
-			    arv = rodaDireitaDuplo(arv);
-		    } else {
-		    	arv = rodaDireita(arv);
-		    }
-	    }
-	    return arv;
-
-	/*
-		updateAltura(arv);
-		if (getBalanco(arv) > 1) {  // arv->esquerda maior
-			if (getBalanco(arv->esquerda) < 0)  // arv->esquerda->direita maior
-				arv->esquerda = rodaDireita(arv->esquerda);  // rotaçao dupla
-			arv = rodaEsquerda(arv);  // simples sem a anterior
-		} else {
-		    if (getBalanco(arv) < 1) {  // arv->direita maior
-			    if (getBalanco(arv->direita) > 0)  // arv->direita->esquerda maior
-				    arv->direita = rodaEsquerda(arv->direita);  // Caso dupla
-			    arv = rodaDireita(arv);
-		    }
-		}
-		return arv; */
-	}
+    NoAVL<T>* equilibra(NoAVL<T> *arv, const T& dado) {
+        int alturaEsquerda = getAltura(arv->getEsquerda());
+        int alturaDireita = getAltura(arv->getDireita());
+        if (dado < *arv->getDado()) {
+            if (alturaEsquerda - alturaDireita == 2) {
+	            if (dado < *(arv->getEsquerda())->getDado()) {
+		            return rodaEsquerda(arv);
+	            } else {
+		            return rodaEsquerdaDuplo(arv);
+	            }
+            }
+        } else {
+            if (alturaDireita - alturaEsquerda == 2) {
+	            if (dado > *(arv->getDireita())->getDado()) {
+    		        return rodaDireita(arv);
+    	        } else {
+	        	    return rodaDireitaDuplo(arv);
+	            }
+            }
+        }
+        return arv;
+    }
 
     NoAVL<T>* minimo(NoAVL<T>* nodo) {
         if (nodo->getEsquerda() != NULL)
